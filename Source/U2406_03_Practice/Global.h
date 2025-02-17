2c98a7013970a87a6d29433e7564c48ba7567603
mxxjee <kmj694288@gmail.com>
2025-02-17 16:03:54 +0900

    projects

---
+++ Source/U2406_03_Practice/Global.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "DrawDebugHelpers.h"
+
+#include  "Kismet/KismetMathLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+
+
+#include "Utilities/CLog.h"
+#include "Utilities/CHelpers.h"
---
+++ Source/U2406_03_Practice/U2406_03_Practice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "U2406_03_Practice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, U2406_03_Practice, "U2406_03_Practice" );
